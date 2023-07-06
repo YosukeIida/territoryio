00f4144ba5f4dcba715e81abbf053cb23c5100e8
YosukeIida <stuiida2002@gmail.com>
2023-07-06 17:44:53 +0900

    0706_[WIP]line,areaのタイルを作っている - プレイヤーとタイルは1:多, 敵，プレーヤーとタイルは多:多の接続になるだろう - GameCharacter.javaのoverlapをプレイヤーの中央がオーバーラップしたときに書き換える必要あり

--- app/src/main/java/com/example/territoryio/controller/BackgroundGrid.java
+++ app/src/main/java/com/example/territoryio/controller/BackgroundGrid.java
@@ -2,17 +2,45 @@ package com.example.territoryio.controller;
 
 import android.util.Log;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class BackgroundGrid{
     private static String TAG = "grid";
 
     public static final int GRID_X = 50;
     public static final int GRID_Y = 50;
 
+
     private int grid[][] = new int[GRID_X][GRID_Y];
 
-    public void move() {
-        Log.i("grid", "grid" + grid);
+
+    // getter
+    public int[][] getGrid() {
+        return grid;
     }
+
+
+    // method
+    public void setCharacterPosition(int x, int y, int characterId) {
+        grid[x][y] = characterId;
+    }
+
+    public void move() {
+        for (int i=0; i<grid[0].length; i++) {
+            for (int j=0; j<grid[1].length; j++) {
+                System.out.print(grid[i][j]);
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+    }
+
+
+
+
 
 
 
--- app/src/main/java/com/example/territoryio/model/World.java
+++ app/src/main/java/com/example/territoryio/model/World.java
@@ -4,43 +4,62 @@ import android.util.Log;
 
 import com.example.territoryio.controller.BackgroundGrid;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class World {
     // 定数
     public static final int WIDTH = 4000;
     public static final int HEIGHT = 4000;
-//    public static final int GRID_X = 50;
-//    public static final int GRID_Y = 50;
 
 
-    private static String TAG = "grid";
+    // モデル
+    Player player;
+    BackgroundGrid backgroundGrid;
 
 
-    // オブジェクト用変数
-    private Player player;
-    private BackgroundGrid backgroundGrid;
 
 //    private int grid[][] = new int[GRID_X][GRID_Y];
 
 
     public World() {
-        // オブジェクトの生成
-        player = new Player(100, 100);
+        // オブジェクトの生成(モデルの取得)
+        player = new Player(100, 100, 1);
         backgroundGrid = new BackgroundGrid();
+
+
+
+        // モデルの接続
 
     }
 
     public void move() {
         // オブジェクトの更新
         player.move();
-//        backgroundGrid.move();
+        backgroundGrid.move();
+
+        backgroundGrid.setCharacterPosition(player.getGridX(), player.getGridY(),player.id);
+
+        for (int i=0; i<backgroundGrid.getGrid()[0].length; i++) {
+            for (int j=0; j<backgroundGrid.getGrid()[1].length; j++) {
+                if (backgroundGrid.getGrid()[i][j] == 1) {
+                    // ステート切り替えをする
+                    // 0だったらlightblue
+                    // 1だったらenemy敵
+//                    lines.add(new Line(i*80, j*80));
+                }
+            }
+        }
 
 
 
 
 
 
-        Log.i("grid", "pcx" + player.getGridX());
-        Log.i("grid", "pcy" + player.getGridY());
+        // 毎回最後にlineを削除
+
+//        Log.i("grid", "pcx" + player.getGridX());
+//        Log.i("grid", "pcy" + player.getGridY());
     }
 
     //Getter
@@ -49,4 +68,6 @@ public class World {
     public Player getPlayer() {
         return player;
     }
+
+
 }
